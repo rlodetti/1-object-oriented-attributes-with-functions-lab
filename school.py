8de881d5887fb8d07d4380da2b1f1a6deb30cbef
Ron Lodetti Jr <ron.lodetti@gmail.com>
2023-07-11 16:16:35 -0400

    finished lab

--- school.py
+++ school.py
@@ -1,1 +1,27 @@
 class School:
+    
+    def __init__(self,name):
+        self.name = name
+        self.roster = {}
+    
+    def add_student(self,name,grade):
+        if grade not in self.roster.keys():
+            students = [name]
+            self.roster[grade] = students
+        else:
+            self.roster[grade].append(name)
+        
+        grades = list(self.roster.keys())
+        grades.sort()
+        sorted_dict = {i: self.roster[i] for i in grades}
+        self.roster = sorted_dict
+        return self.roster
+
+    def grade(self,grade):
+        return self.roster.get(grade)
+    
+    def sort_roster(self):
+        for key,val in self.roster.items():
+            self.roster[key] = sorted(val)
+        return self.roster
+        
